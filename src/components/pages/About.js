5b3a19c7e6953c9cd34d562189aa9889f35323c3
kiserr10 <kiserr10@gmail.com>
2019-07-08 21:11:52 -0600

    added images and main styles for pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import portrait from '../../assets/images/ross_face.png'
+
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>About PAGE</h1>
+      <div className="about-wrapper">
+        <img className="art-face" src={portrait} alt="Ross Abstract Face" />
       </div>
     )
   }
