e4000820ec737bb9f3cf6995d1cf5ac084b4643e
Ross Daniel Kiser <kiserr10@gmail.com>
2019-07-12 15:00:57 -0600

    added remaining pages with rough boiler plate

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import portrait from '../../assets/images/ross_face.png'
+import data from '../../assets/json/core_content.json';
 
 
 class About extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
   render() {
     return (
       <div className="about-wrapper">
         <div className="about-bio">
-          <p>Long before I became a web developer, technology has been central to my creative practice. I self-taught my way into audio engineering and started my own electronic music record label which has gone on to sign several internationally acclaimed acts. After developing this part of my life for over half a decade, I encountered the world of code. A deeper dimension of technology with which I instantly clicked, it was the perfect alignment of design, creativity, and problem solving that I had been seeking. In 2017, I decided to dive in fully and immerse myself in the culture of development. I continue to use these skillsets as a creative avenue to influence the worlds of my other passions, such as music, rock climbing, and food culture.</p>
+          <h1>me.</h1>
+          <p>{data.bio}</p>
         </div>
         <img className="art-face" src={portrait} alt="Ross Abstract Face" />
       </div>
---
+++ src/components/pages/Tech.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Tech = () => {
+  return (
+    <div>
+      <h1>Tech</h1>
+    </div>
+  )
+}
+export default Tech
