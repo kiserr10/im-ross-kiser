8ca7ec16d8f01e7f3169f2edf283643e5fe9a8c0
kiserr10 <kiserr10@gmail.com>
2019-08-15 18:01:53 -0600

    fixed burger menu stylewsd

--- src/components/shared/BurgerMenu/index.js
+++ src/components/shared/BurgerMenu/index.js
@@ -39,7 +39,7 @@ class BurgerMenu extends Component {
       <div className={styles[`burger-menu`]} onClick={this.onClick}>
         <svg id="burger" className="openmenu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
           <path ref={topbar => this.topbar = topbar} className="top" d="M0 9h30v2H0z"/>
-          <line ref={midbar => this.midbar = midbar} className="mid" x1="0" y1="15" x2="30" y2="15" stroke="#F74753" strokeWidth="2" vectorEffect="non-scaling-stroke"/>
+          <line ref={midbar => this.midbar = midbar} className="mid" x1="0" y1="15" x2="30" y2="15" stroke="#F74753" strokeWidth="3" vectorEffect="non-scaling-stroke"/>
           <path ref={botbar => this.botbar = botbar} className="bot" d="M0 19h30v2H0z"/>
         </svg>
       </div>
